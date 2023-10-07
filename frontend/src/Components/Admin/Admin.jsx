dd7bd667e4bf91186fba912b7baf8f9cb4c9347c
siva <sivamani_k@srmap.edu.in>
2023-10-07 12:08:34 +0530

    update

--- frontend/src/Components/Admin/Admin.jsx
+++ frontend/src/Components/Admin/Admin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Authform from '../Auth/Authform'
 
 const Admin = () => {
   return (
     <div>
-      Admin
+      <Authform/>
     </div>
   )
 }
--- frontend/src/Components/Auth/Authform.jsx
+++ frontend/src/Components/Auth/Authform.jsx
@@ -1,11 +1,76 @@
-import { Dialog, Typography } from '@mui/material'
-import React from 'react'
-import './Authform.css'
-const Authform = () => {
-  return <Dialog open={true}>
-    <Typography variant='h4' className='auth1`'>Login</Typography>
-    <form></form>
-  </Dialog>
-}
+import { Box, Button, Dialog, FormLabel, IconButton, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import './Authform.css';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
-export default Authform
+const labelstyle = { mt: 1, mb: 1 };
+
+const Authform = () => {
+  const [input, setinput] = useState({
+    name: "", email: "", password: ""
+  })
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleFormSwitch = () => {
+    setIsSignup(!isSignup);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input)
+  };
+  const handleChange = (e)=>{
+    setinput((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value,
+    }))
+  }
+  return (
+    <Dialog PaperProps={{ style: { borderRadius: 20 } }} open={true}>
+      <Box sx={{ ml: 'auto', padding: 1 }}>
+        <IconButton>
+          <CloseRoundedIcon />
+        </IconButton>
+      </Box>
+      <Typography variant='h4' className='auth1'>
+        {isSignup ? 'Signup' : 'Login'}
+      </Typography>
+      <form className='form' onSubmit={handleSubmit} >
+        <Box className='formContainer'>
+          {isSignup && (
+            <>
+              <FormLabel className={labelstyle}>Name</FormLabel>
+              <TextField value={input.name}
+                onChange={handleChange}
+                margin='normal' variant='standard' type='text' name='name' />
+            </>
+          )}
+          <FormLabel sx={labelstyle}>Email</FormLabel>
+          <TextField margin='normal' value={input.email}
+            onChange={handleChange} variant='standard' type='email' name='email' />
+          <FormLabel sx={labelstyle}>Password</FormLabel>
+          <TextField margin='normal' value={input.password}
+            onChange={handleChange} variant='standard' type='password' name='password' />
+          <Button
+            sx={{ mt: 2, borderRadius: 10, bgcolor: '#10b5cb' }}
+            type='submit'
+            fullWidth
+            variant='contained'
+          >
+            {isSignup ? 'Signup' : 'Login'}
+          </Button>
+          <Button
+            onClick={handleFormSwitch}
+            sx={{ mt: 2, borderRadius: 10 }}
+            type='button'
+            fullWidth
+          >
+            {isSignup ? 'Login' : 'Signup'}
+          </Button>
+        </Box>
+      </form>
+    </Dialog>
+  );
+};
+
+export default Authform;
