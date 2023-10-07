12e8ed3d1d1a8e5ab5d697859652240abd70bc2f
siva <sivamani_k@srmap.edu.in>
2023-10-07 10:46:36 +0530

    update

---
+++ frontend/src/Components/Auth/Authform.jsx
@@ -0,0 +1,11 @@
+import { Dialog, Typography } from '@mui/material'
+import React from 'react'
+import './Authform.css'
+const Authform = () => {
+  return <Dialog open={true}>
+    <Typography variant='h4' className='auth1`'>Login</Typography>
+    <form></form>
+  </Dialog>
+}
+
+export default Authform
