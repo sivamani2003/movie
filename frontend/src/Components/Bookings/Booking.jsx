106e7ab2254ecafc00f12bee3346c35dcf8c0feb
siva <sivamani_k@srmap.edu.in>
2023-10-09 09:35:54 +0530

    update

--- frontend/src/Components/Bookings/Booking.jsx
+++ frontend/src/Components/Bookings/Booking.jsx
@@ -1,20 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getMovieDeatils } from '../../apis/api'
+import React, { useEffect, useState } from 'react';
+import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieDeatils } from '../../apis/api';
+import { Box, Typography } from '@mui/material';
+import './booking.css';
 
 const Booking = () => {
-    const [movie,setmovie]= useState()
-    const id = useParams().id
-    console.log(id)
-    useEffect(()=>{
-        getMovieDeatils(id).then((res)=>setmovie(res.movie)).catch((err)=>console.log(err))
-    },[id])
-    console.log(movie)
-  return (
-    <div>
-      Booking
-    </div>
-  )
-}
+  const [movie, setmovie] = useState();
+  const id = useParams().id;
 
-export default Booking
+  useEffect(() => {
+    getMovieDeatils(id)
+      .then((res) => setmovie(res.movie))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return (
+    <div className="booking-container">
+      {movie && (
+        <Fragment>
+          <Typography className='booking1' variant='h4'>
+            Book Tickets for Movie: {movie.title}
+          </Typography>
+          <Box className="book2">
+            <Box className="book3">
+              <img width="80%" height={"300px"} className='book4' src={movie.postUrl} alt={movie.title} />
+            </Box>
+            <Box width={"80%"}>
+
+            </Box>
+          </Box>
+        </Fragment>
+      )}
+    </div>
+  );
+};
+
+export default Booking;
